cc7f79e86a2b6cc5866cf00be968ba999c3b4fb1
liangchenming <15900757790@163.com>
2023-04-18 12:15:55 +0800

    变更

--- common/send_requests.py
+++
@@ -1,9 +0,0 @@
-import requests
-from requests.auth import HTTPBasicAuth
-
-class SendRequests:
-    sess = requests.Session()
-
-    def all_send_requests(self, method, url, **kwargs):
-        res = SendRequests.sess.request(method, url, auth=HTTPBasicAuth('user', 'dqwR5,2c.qdaeyq3oL'), **kwargs)
-        return res
